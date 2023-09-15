a1da06bfa60f8b23fb5b14b634aa9abf401b3ac0
Erdabi <yusuf.khurshid@gmx.ch>
2023-09-15 14:53:23 +0200

    unclean_code_products initialised :tada:

---
+++ unclean_code_products.js
@@ -0,0 +1,34 @@
+const products = [
+  { productid: 1, name: "Laptop", cost: 999 },
+  { productid: 2, name: "Smartphone", cost: 599 },
+  { productid: 3, name: "Headphones", cost: 199 },
+];
+
+function getProduct(id) {
+  for (let product of products) {
+    if (product.productid == id) 
+    return product;
+  }
+  return null;
+}
+
+function addProduct(name, cost) {
+  let maxId = 0;
+  for (let product of products) {
+    if (product.productid > maxId) maxId = product.productid;
+  }
+  const newProduct = { id: maxId + 1, name: name, cost: cost };
+  const updatedProduct = [...products, newProduct];
+  return updatedProduct;
+}
+
+function logProducts() {
+  for (let product of products) {
+    console.log(product.name + " costs " + product.cost + "€");
+  }
+}
+
+let myProduct = getProduct(2);
+console.log(myProduct);
+addProduct("Tablet", 399);
+logProducts();
